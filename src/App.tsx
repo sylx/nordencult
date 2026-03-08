3a72ac629a2f1dbbb7204f4a6e1e84f46e8e188f
sylx <sharpoon@gmail.com>
2026-03-08 23:13:54 +0900

    マップオーバーレイに情報ウィンドウコンポーネントを追加し、スタイルと機能を実装しました。また、関連する画像ファイルを追加しました。

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css'
 import Map from './map/components/Map'
+import InfoWindow from './ui/components/InfoWindow'
 
 function App() {
   return (
     <>
     <Map />
     <div className="ui">
-        <h1>Nordencult</h1>
+        <InfoWindow title="Welcome to Norden Cult!" width={600} height={"auto"} resizeable>
+          <p>Explore the map and click on the places to learn more about them. there are many secrets to discover!</p>
+          <p>Explore the map and click on the places to learn more about them.</p>
+          <p>Explore the map and click on the places to learn more about them.</p>
+          <p>Explore the map and click on the places to learn more about them.</p>
+          <p>Explore the map and click on the places to learn more about them.</p>
+          <p>Explore the map and click on the places to learn more about them.</p>
+        </InfoWindow>
     </div>
 
     </>
---
+++ src/ui/components/InfoWindow.tsx
@@ -0,0 +1,54 @@
+import './InfoWindow.css'
+
+export interface InfoWindowProps {
+  title: string
+  children: React.ReactNode
+  className?: string
+  style?: React.CSSProperties
+  resizeable?: boolean
+  width?: number
+  height?: number | 'auto',
+  x?: number
+  y?: number
+}
+
+export default function InfoWindow({
+  title,
+  children,
+  className,
+  style,
+  resizeable = false,
+  width = 300,
+  height = 'auto',
+  x = 0,
+  y = 0,
+}: InfoWindowProps) {
+
+  const contentHeight = height
+  return (
+    <div
+      className={`info-window ${resizeable ? 'resizeable' : ''} ${className || ''}`}
+      style={{ width, height,left: x, top: y+50, ...style }}
+    >
+      <div className="info-window-bg-container">
+        <div className="info-window-corner --left-top" />
+        <div className="info-window-top"></div>
+        <div className="info-window-corner --right-top" />
+        <div className="info-window-left" />
+        <div className="info-window-center"></div>
+        <div className="info-window-right" />
+        <div className="info-window-corner --left-bottom" />
+        <div className="info-window-bottom" />
+        <div className="info-window-corner --right-bottom" />
+      </div>
+      <div className="info-window-title">
+        <span className="info-window-title-corner" />
+        <span className="info-window-title-text">{title}</span>
+        <span className="info-window-title-corner" />
+      </div>
+      <div className="info-window-content">
+        {children}
+      </div>
+    </div>
+  )
+}
